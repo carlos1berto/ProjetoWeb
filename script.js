f96f32b0ce60d301ec0e3f5c79a7b4e4d8a3fedb
CArlos Humberto <carlos.hcs1998@gmail.com>
2021-01-20 15:07:33 -0300

    frontend ecommerce

---
+++ script.js
@@ -0,0 +1,12 @@
+var named="";
+var senha = "";
+function bthover (){
+  
+        document.getElementById('entrar').style.backgroundColor='#D05700'
+    
+}
+function cadastro(){
+        document.getElementById('containerLogin').style.display="none";
+        document.getElementById('containerCadastro').style.display="flex"
+        document.getElementById('containerCadastro').style.flexDirection="column"
+}
